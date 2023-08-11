71010b203605544c7793a00e2bdda2f3789eeb66
detailcore <savemysoulok@yandex.ru>
2023-08-11 09:17:40 +0400

    show Notify use Cookie

--- app.vue
+++ app.vue
@@ -14,6 +14,27 @@ import { storeToRefs } from 'pinia'
 import { useModalStore } from '@/stores/modal'
 
 const { isOpen } = storeToRefs(useModalStore())
+
+const showNotifyCookie = () => {
+  const cookieAuth = useCookie('auth')
+  const cookiePrompt = useCookie('cookiePrompt')
+  if(!cookiePrompt.value && cookieAuth.value == '1') {
+    ElNotification({
+      title: 'Мы используем cookie.',
+      message: 'Вы можете запретить их в настройках браузера, но тогда сайт будет работать не корректно!',
+      position: 'bottom-right',
+      duration: 0,
+      onClose() {
+        const cookiePrompt = useCookie('cookiePrompt', { maxAge: 60 * 60 * 24 * 180 })
+        cookiePrompt.value = '1'
+      },
+    })
+  }
+}
+
+onMounted(() => {
+  showNotifyCookie()
+})
 </script>
 
 
